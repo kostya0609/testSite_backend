9f3f6d96d92cfb5d09f133020ebaf174d86d813f
bsi24 <krivopuskkv@khv-it-d-61.bsi.local>
2023-08-28 10:45:51 +1000

    2023_08_28

--- Routes/routes.php
+++ Routes/routes.php
@@ -1,14 +1,16 @@
 <?php
+namespace App\Modules\TestSite\Controllers\v1;
 use Illuminate\Support\Facades\Route;
-use App\Modules\Site\Controllers;
 
-Route::prefix('site')
-    ->group(function(){
 
-        Route::prefix('questions')->group(function (){
-            Route::post('list',Controllers\QuestionController::class.'@list');
+Route::prefix('v1')
+    ->group(function (){
+        Route::prefix('site')
+            ->group(function(){
+                Route::prefix('questions')->group(function (){
+                    Route::post('list',Controllers\v1\QuestionController::class.'@list');
+                });
+            });
+});
 
-        });
-
-    });
 
