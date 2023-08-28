f33039f428ad6f63ebce3df9c2848a720912c842
bsi24 <krivopuskkv@khv-it-d-61.bsi.local>
2023-08-28 10:33:54 +1000

    2023_08_28

--- Controllers/QuestionController.php
+++ Controllers/QuestionController.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class InfoController extends Controller {
+class QuestionController extends Controller {
 
-    public function get(Request $request){
+    public function list(Request $request){
 
         return response()->json(['status' => 'success', 'data' => [
             'mok_data_new' => 'it is ok new'
--- Routes/routes.php
+++ Routes/routes.php
@@ -5,8 +5,8 @@ use App\Modules\Site\Controllers;
 Route::prefix('site')
     ->group(function(){
 
-        Route::prefix('info')->group(function (){
-            Route::post('get',Controllers\InfoController::class.'@get');
+        Route::prefix('questions')->group(function (){
+            Route::post('list',Controllers\QuestionController::class.'@list');
 
         });
 
